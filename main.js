626cf3bde6f4eeb06565c189c9d10e3043ea941b
Danielle Carrick <daniellecarrick@Danielles-MacBook-Air.local>
2017-02-27 12:15:00 +0200

    some but not all tests passing

--- main.js
+++ main.js
@@ -19,3 +19,61 @@ var compose = function (targetObj) {
     return targetObj;
   }
 };
+
+var refueler = function(state) {
+  return {
+    refuel: function() {
+      state.gas += 1;
+    }
+  };
+}
+
+var shooter = function(state) {
+  return {
+    shoot: function() {
+      this.dead = true; //check later
+    }
+  }
+}
+
+var flyer = function(state) {
+  return {
+    fly: function() {
+      if (state.gas > 0) {
+        state.position += 10;
+        state.gas -= 1;
+      } else if (state.gas < 1) {
+       // state.gas -= 1;
+      }
+    }
+  }
+}
+
+var saver = function(state) {
+  return {
+    save: function() {
+      this.dead = false;
+    }
+  }
+}
+
+var driver = function(state) {
+  return {
+    drive: function() {
+      if (state.gas > 0) {
+        state.position += 5;
+        state.gas -= 1;
+      }
+    }
+  }
+}
+
+var plane = function(name) {
+
+  return compose(
+    state,
+    refueler(state),
+    flyer(state),
+    shooter(state)
+  )
+}
